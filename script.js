6177719ebc94e4261174c3373777ca1c67fbd0fe
mazen hosaisy <mazen hosaisy>
2016-08-24 19:55:22 +0300

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+var mazen={height:180,weight:92,age:30};
+var diana={height:170,weight:60,age:19};
+var aya={height:168,weight:55,age:26};
+var course=[diana,aya,mazen];
+for(var i=0;i<4;i++){
+    course[i].height=175;
+}
